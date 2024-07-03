455acdb475d28fc2e241a9d04feee3f1131e7ae8
steve <stevegreghatch@gmail.com>
2024-07-03 12:40:22 -0400

    updated DbClient.py

--- src/main/mongo/DbClient.py
+++ src/main/mongo/DbClient.py
@@ -1,12 +1,32 @@
-from dotenv import dotenv_values
+import os
+import logging
 from pymongo import MongoClient
+from dotenv import dotenv_values
 
-env_vars = dotenv_values(".env")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+)
+logger = logging.getLogger(__name__)
 
-MONGO_USERNAME = env_vars.get("MONGO_USERNAME")
-MONGO_PASSWORD = env_vars.get("MONGO_PASSWORD")
-MONGO_HOST = env_vars.get("MONGO_HOST")
-MONGO_URI = "mongodb+srv://{}:{}.{}".format(MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST)
 
-client = MongoClient(MONGO_URI)
-db = client["space_exploration"]
+def load_env():
+    if 'KUBERNETES_SERVICE_HOST' not in os.environ:
+        env_vars = dotenv_values(".env")
+        os.environ.update(env_vars)
+
+
+load_env()
+
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_HOST = os.getenv("MONGO_HOST")
+
+MONGO_URI = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}.{MONGO_HOST}"
+
+try:
+    client = MongoClient(MONGO_URI)
+    db = client["space_exploration"]
+    logger.info('Connected successfully to MongoDB!')
+except Exception as e:
+    logger.info(f'Error connecting to MongoDB: {str(e)}')
