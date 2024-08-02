7498638dc296afccf7e7eb2d47c32dca1c654e0d
steve <stevegreghatch@gmail.com>
2024-08-02 09:56:05 -0400

    updated setup.py python v

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    python_requires="3.12, <4",
+    python_requires='>=3.12, <4',
     include_package_data=True,
     install_requires=[
         'fastapi'
